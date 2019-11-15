ca0cc79a94da0a69aa175bebd775c3c16db34577
Domenico <ferraro.domenico125@gmail.com>
2019-11-15 11:59:59 +0000

    Add bin logic

---
+++ SustainUCD/app/src/main/java/com/example/sustainucd/AddBinActivity.java
@@ -0,0 +1,54 @@
+package com.example.sustainucd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AddBinActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView binImageView;
+    private TextView locationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bin);
+        binImageView = findViewById(R.id.binImageView);
+        locationTextView = findViewById(R.id.locationTextView);
+    }
+
+    public void TakePhoto(View view)
+    {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private String GetLocation()
+    {
+        return "";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            if (imageBitmap != null) {
+                binImageView.setImageBitmap(imageBitmap);
+                String locationString = GetLocation();
+                locationTextView.setText(String.format("Location: \n%s", locationString));
+            }
+        }
+    }
+}
